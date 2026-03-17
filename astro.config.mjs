d04f870354d9451797a17ddb8f622eef7e31d4a9
Laurence <gkakese@gmail.com>
2026-03-17 02:38:20 +0200

    Configure Astro for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://gkakese.github.io",
+  base: "/copakgt-website/",
   vite: {
     plugins: [tailwindcss()]
   }
