f83cac992be67167d6bdc1557b66433f12535120
Laurence <gkakese@gmail.com>
2026-03-22 19:19:37 +0200

    Fix GitHub Pages site URL for copakgt deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://copakgt.github.io',
+  site: 'https://adminbrighthorizon-hash.github.io',
   base: '/copakgt-website',
 });
